6ef1c8da27aabdf3af47646f0bec57fe27a66166
mustafaoncu <mustafaoncu815@gmail.com>
2024-03-20 03:27:46 +0300

    add: filter method with all parameters in kwargs

--- dict_maker.py
+++ dict_maker.py
@@ -52,7 +52,7 @@ def query_to_dict(session, table_name, field_name, field_value):
         session.close()
 
 
-def insert_data(session, table_name, **data):
+def insert_data(session, table_name, **kwargs):
     """
     Inserts data into a table.
     param: session: SQLAlchemy session object
@@ -61,13 +61,39 @@ def insert_data(session, table_name, **data):
     """
     try:
         table = getattr(Base.classes, table_name)
-        new_record = table(**data)
+        new_record = table(**kwargs)
         session.add(new_record)
         session.commit()
         print(f"Data inserted successfully into {table_name}.")
     except Exception as e:
         session.rollback()
         print(f"Error inserting data into {table_name}: {e}")
+    finally:
+        session.close()
+
+
+# kwargs which included table_name
+def filter_records_with_table_name(session, **kwargs):
+    """
+    Queries and filters a table based on given keyword arguments including the table name.
+    param session: SQLAlchemy session object
+    param kwargs: keyword arguments representing the table name and filter conditions
+    return: a list of dictionaries, each representing a row in the table
+    """
+    table_name = kwargs.pop('table_name', None)
+    if not table_name:
+        print("Table name must be provided with 'table_name' key.")
+        return []
+    try:
+        table = getattr(Base.classes, table_name)
+        query = session.query(table)
+        for key, value in kwargs.items():
+            query = query.filter(getattr(table, key) == value)
+        results = query.all()
+        return [result.to_dict() for result in results]
+    except Exception as e:
+        print(f"An error occurred while filtering records: {e}")
+        return []
     finally:
         session.close()
 
@@ -94,8 +120,9 @@ def filter_records(session, table_name, **kwargs):
         session.close()
 
 
-
 if __name__ == '__main__':
     session = Session()
-    result_dict = query_to_dict(session, 'Lighting', 'LightingNames', "('0_configurator_cameras', '0_lighting')")
+    result_dict = query_to_dict(session, 'Lighting', 'LightingNames', "(0_configurator_cameras, 0_lighting)")
+    result_new_filter_method = filter_records(session, 'Lighting', **result_dict[0])
     print(result_dict)
+    print(result_new_filter_method)

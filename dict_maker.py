de16ae8126fb703f25eddc4ad6c68d22c3e0ffc7
mustafaoncu <mustafaoncu815@gmail.com>
2024-03-20 03:08:41 +0300

    add: new filter and insert methods

--- dict_maker.py
+++ dict_maker.py
@@ -33,6 +33,7 @@ class ToDictMixin(object):
 for class_ in Base.classes:
     class_.__bases__ = (ToDictMixin,) + class_.__bases__
 
+
 def query_to_dict(session, table_name, field_name, field_value):
     """
     Queries a table in the database and convert to dict
@@ -49,6 +50,49 @@ def query_to_dict(session, table_name, field_name, field_value):
 
     finally:
         session.close()
+
+
+def insert_data(session, table_name, **data):
+    """
+    Inserts data into a table.
+    param: session: SQLAlchemy session object
+    param: table_name: the name of the table to insert data into
+    param: data: keyword arguments representing the data to insert
+    """
+    try:
+        table = getattr(Base.classes, table_name)
+        new_record = table(**data)
+        session.add(new_record)
+        session.commit()
+        print(f"Data inserted successfully into {table_name}.")
+    except Exception as e:
+        session.rollback()
+        print(f"Error inserting data into {table_name}: {e}")
+    finally:
+        session.close()
+
+
+def filter_records(session, table_name, **kwargs):
+    """
+    Queries and filters a table based on given keyword arguments.
+    param session: SQLAlchemy session object
+    param table_name: the name of the table to query
+    param kwargs: keyword arguments representing the filter conditions
+    return: a list of dictionaries, each representing a row in the table
+    """
+    try:
+        table = getattr(Base.classes, table_name)
+        query = session.query(table)
+        for key, value in kwargs.items():
+            query = query.filter(getattr(table, key) == value)
+        results = query.all()
+        return [result.to_dict() for result in results]
+    except Exception as e:
+        print(f"An error occurred while filtering records: {e}")
+        return []
+    finally:
+        session.close()
+
 
 
 if __name__ == '__main__':
